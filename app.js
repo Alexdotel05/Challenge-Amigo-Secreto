c34acd8673b04a11eb910c97dec6854e9b781bba
Alexander Ferreras Dotel <alexdotel05@gmail.com>
2025-08-31 23:21:08 -0400

    Actualiza el codigo a la última versión.

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 let listaDeAmigos = [];
-let nombre = "";
+
 
 
 function agregarAmigo() {
-    let imputAmigo = document.querySelector('#amigo').value;
-    nombre = imputAmigo;
-    if (nombre === " ") {
-        alert ("Por favor, inserte un nombre.");
-      
+    let nombre = document.querySelector('#amigo').value;
+
+    if (nombre.trim() === "") {
+        alert("Por favor, inserte un nombre."); 
+    } else if (listaDeAmigos.includes(nombre)) {
+        alert("El nombre ingresado ya existe, por favor ingrese otro nombre válido.");}
+            else {listaDeAmigos.push(nombre);
+
+        console.log(listaDeAmigos);
+       document.getElementById('amigo').value = "";
+       actualizarListaDeAmigos();
     }
 }
 
+function actualizarListaDeAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";  // Limpiar la lista antes de agregar nuevos elementos
 
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li= document.createElement('li');
+
+        li.textContent = listaDeAmigos[i];
+        lista.appendChild(li);
+        
+    }
+
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random()*listaDeAmigos.length);
+    let amigoSorteado = listaDeAmigos[indiceAleatorio];
+    document.getElementById('resultado').innerHTML = "El amigo secreto es:" + amigoSorteado; 
+}
+  
+
+function reiniciarJuego() {
+    listaDeAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = "";
+    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('amigo').value = "";
+}
